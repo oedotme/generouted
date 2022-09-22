3e2e718533e77632d1fd045bb2294f47694f700b
oedotme <oedotme@gmail.com>
2022-09-22 22:36:06 +0200

    refactor: add generate regular routes utility

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { Fragment } from 'react'
 import { LoaderFn, Outlet, ReactLocation, Route, Router, RouterProps } from '@tanstack/react-location'
 
-import { generatePreservedRoutes } from './utils'
+import { generatePreservedRoutes, generateRegularRoutes } from './utils'
 
 type Element = () => JSX.Element
 type Module = { default: Element; Loader: LoaderFn; Pending: Element; Failure: Element }
@@ -11,57 +11,12 @@ const ROUTES = import.meta.glob<Module>(['/src/pages/**/[\\w[]*.{jsx,tsx}', '!**
 
 const preservedRoutes = generatePreservedRoutes<Element>(PRESERVED)
 
-const regularRoutes = Object.keys(ROUTES).reduce<Route[]>((routes, key) => {
-  const module = ROUTES[key]
-  const route: Route = {
-    element: () => module().then((mod) => (mod?.default ? <mod.default /> : <></>)),
-    loader: async (...args) => module().then((mod) => mod?.Loader?.(...args)),
-    pendingElement: async () => module().then((mod) => (mod?.Pending ? <mod.Pending /> : null)),
-    errorElement: async () => module().then((mod) => (mod?.Failure ? <mod.Failure /> : null)),
-  }
-
-  const segments = key
-    .replace(...patterns.clean)
-    .replace(...patterns.catch)
-    .replace(...patterns.param)
-    .split('/')
-    .filter(Boolean)
-
-  segments.reduce((parent, segment, index) => {
-    const path = segment.replace(...patterns.index)
-    const root = index === 0
-    const leaf = index === segments.length - 1 && segments.length > 1
-    const node = !root && !leaf
-    const insert = /^\w|\//.test(path) ? 'unshift' : 'push'
-
-    if (root) {
-      const dynamic = path.startsWith(':') || path === '*'
-      if (dynamic) return parent
-
-      const last = segments.length === 1
-      if (last) {
-        routes.push({ path, ...route })
-        return parent
-      }
-    }
-
-    if (root || node) {
-      const current = root ? routes : parent.children
-      const found = current?.find((route) => route.path === path)
-      if (found) found.children ??= []
-      else current?.[insert]({ path, children: [] })
-      return found || (current?.[insert === 'unshift' ? 0 : current.length - 1] as Route)
-    }
-
-    if (leaf) {
-      parent?.children?.[insert]({ path, ...route })
-    }
-
-    return parent
-  }, {} as Route)
-
-  return routes
-}, [])
+const regularRoutes = generateRegularRoutes<Route, () => Promise<Module>>(ROUTES, (module) => ({
+  element: () => module().then((mod) => (mod?.default ? <mod.default /> : <></>)),
+  loader: async (...args) => module().then((mod) => mod?.Loader?.(...args)),
+  pendingElement: async () => module().then((mod) => (mod?.Pending ? <mod.Pending /> : null)),
+  errorElement: async () => module().then((mod) => (mod?.Failure ? <mod.Failure /> : null)),
+}))
 
 const App = preservedRoutes?.['_app'] || Fragment
 const NotFound = preservedRoutes?.['404'] || Fragment
--- src/utils.tsx
+++ src/utils.tsx
@@ -6,10 +6,60 @@ const patterns = {
 } as const
 
 type PreservedKey = '_app' | '404'
+type BaseRoute = { path?: string; children?: BaseRoute[] } & Record<string, any>
 
 export const generatePreservedRoutes = <T,>(routes: Record<string, T | any>): Partial<Record<PreservedKey, T>> => {
   return Object.keys(routes).reduce((result, current) => {
     const path = current.replace(...patterns.clean)
     return { ...result, [path]: routes[current]?.default }
   }, {})
+}
+
+export const generateRegularRoutes = <T, M>(routes: Record<string, any>, buildRoute: (module: M) => T) => {
+  return Object.keys(routes).reduce<BaseRoute[]>((result, current) => {
+    const module = routes[current]
+    const route = buildRoute(module)
+
+    const segments = current
+      .replace(...patterns.clean)
+      .replace(...patterns.catch)
+      .replace(...patterns.param)
+      .split('/')
+      .filter(Boolean)
+
+    segments.reduce((parent, segment, index) => {
+      const path = segment.replace(...patterns.index)
+      const root = index === 0
+      const leaf = index === segments.length - 1 && segments.length > 1
+      const node = !root && !leaf
+      const insert = /^\w|\//.test(path) ? 'unshift' : 'push'
+
+      if (root) {
+        const dynamic = path.startsWith(':') || path === '*'
+        if (dynamic) return parent
+
+        const last = segments.length === 1
+        if (last) {
+          result.push({ path, ...route })
+          return parent
+        }
+      }
+
+      if (root || node) {
+        const current = root ? result : parent.children
+        const found = current?.find((route) => route.path === path)
+        if (found) found.children ??= []
+        else current?.[insert]({ path, children: [] })
+        return found || (current?.[insert === 'unshift' ? 0 : current.length - 1] as BaseRoute)
+      }
+
+      if (leaf) {
+        parent?.children?.[insert]({ path, ...route })
+      }
+
+      return parent
+    }, {} as BaseRoute)
+
+    return result
+  }, [])
 }
