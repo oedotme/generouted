b3e7e805f91f2c5665e398cfe290ac049d5b760a
oedotme <oedotme@gmail.com>
2022-09-23 08:12:06 +0200

    chore: move current examples into react-location

--- examples/react-location/basic/src/main.tsx
+++ examples/react-location/basic/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { Routes } from 'generouted'
+import { Routes } from 'generouted/react-location'
 
 const container = document.getElementById('app')!
 createRoot(container).render(<Routes />)
