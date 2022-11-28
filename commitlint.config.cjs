fdb0b31bc257933b09afc261aec05a5cf2c6c2c6
oedotme <oedotme@gmail.com>
2022-11-28 23:35:09 +0200

    chore: include release in commitlint type enum

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,10 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'release', 'revert', 'style', 'test'],
+    ],
+  },
+}
