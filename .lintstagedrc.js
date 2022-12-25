567a50f086fc5394050916e8171637288669c98b
oedotme <oedotme@gmail.com>
2022-12-25 08:47:28 +0200

    chore: move generouted to packages; setup changesets

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{ts,tsx}': () => 'tsc -p tsconfig.json',
+  './{packages,plugins}/**/*.{ts,tsx}': () => 'pnpm -r type-check',
   '*.{css,html,json,md,mdx,js,jsx,ts,tsx}': 'prettier --write',
 }
