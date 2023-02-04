c9ac77dbb7037d67b47a505148a49a505fea3e66
oedotme <oedotme@gmail.com>
2023-02-04 08:46:18 +0100

    chore: add examples and shared to lint-staged type-check

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  './{packages,plugins}/**/*.{ts,tsx}': () => 'pnpm -r type-check',
+  './{examples,packages,plugins,shared}/**/*.{ts,tsx}': () => 'pnpm -r type-check',
   '*.{css,html,json,md,mdx,js,jsx,ts,tsx}': 'prettier --write',
 }
