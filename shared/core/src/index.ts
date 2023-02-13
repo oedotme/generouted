cfb0a64969e89ca4ba680090d419c23d89ed437e
brotherko <bruvahko@gmail.com>
2023-02-13 23:03:07 +0800

    fix: react router plugin (#47)
    * fix: support typed export functions
    
    * fix: loader not receiving args
    
    * Update generate.ts
    
    * fix: update template to align with react-router
    
    * fix:  import
    
    * Update shared/core/src/index.ts
    
    Co-authored-by: Omar Elhawary <oedotme@gmail.com>
    
    * Update plugins/react-router/src/generate.ts
    
    Co-authored-by: Omar Elhawary <oedotme@gmail.com>
    
    ---------
    
    Co-authored-by: Marty Ko <marty.ko@kadensacapital.com>
    Co-authored-by: Omar Elhawary <oedotme@gmail.com>

--- shared/core/src/index.ts
+++ shared/core/src/index.ts
@@ -12,10 +12,10 @@ const getRouteId = (path: string) => path.replace(...patterns.route).replace(/\W
 
 const getRouteExports = (content: string) => ({
   default: /^export\s+default\s/gm.test(content),
-  loader: /^export\s+(const|function)\s+Loader(\s|\()/gm.test(content),
-  action: /^export\s+(const|function)\s+Action(\s|\()/gm.test(content),
-  pending: /^export\s+(const|function)\s+Pending(\s|\()/gm.test(content),
-  catch_: /^export\s+(const|function)\s+Catch(\s|\()/gm.test(content),
+  loader: /^export\s+(const|function|let)\s+Loader\W/gm.test(content),
+  action: /^export\s+(const|function|let)\s+Action\W/gm.test(content),
+  pending: /^export\s+(const|function|let)\s+Pending\W/gm.test(content),
+  catch_: /^export\s+(const|function|let)\s+Catch\W/gm.test(content),
 })
 
 type BaseRoute = { id?: string; path?: string; children?: BaseRoute[] } & Record<string, any>
