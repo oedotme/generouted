a630289d3085671c3c329c268bd7d0765b24ff2a
Rodrigo Sanabria <rodrigo@sanabria.dev>
2023-08-22 18:12:57 +0200

    fix: update import to @tanstack/react-router (#118)

--- packages/tanstack-react-router/src/template.ts
+++ packages/tanstack-react-router/src/template.ts
@@ -1,6 +1,6 @@
 export const template = `// Generouted, changes to this file will be overriden
 import { Fragment } from 'react'// actions-imports// loaders-imports
-import { lazyRouteComponent, Outlet, Router, RootRoute, Route, RouterProvider } from '@tanstack/router'
+import { lazyRouteComponent, Outlet, Router, RootRoute, Route, RouterProvider } from '@tanstack/react-router'
 
 // imports
 
@@ -14,7 +14,7 @@ const config = root.addChildren([
 const router = new Router({ routeTree: config })
 export const Routes = () => <RouterProvider router={router} />
 
-declare module '@tanstack/router' {
+declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router
   }
