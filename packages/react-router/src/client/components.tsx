bbd27cd2d95569cf30724f14651f633fe4558e69
oedotme <oedotme@gmail.com>
2024-07-08 18:11:48 +0200

    feat: forward ref for react-router link

--- packages/react-router/src/client/components.tsx
+++ packages/react-router/src/client/components.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { generatePath, Link, Navigate } from 'react-router-dom'
 import { LinkProps, To } from './types'
 
+type LinkRef = React.ForwardedRef<HTMLAnchorElement>
+
 export const components = <Path extends string, Params extends Record<string, any>>() => {
   return {
-    Link: <P extends Path | To<Path>>({ to, params, ...props }: LinkProps<P, Params>) => {
+    Link: forwardRef(<P extends Path | To<Path>>({ to, params, ...props }: LinkProps<P, Params>, ref: LinkRef) => {
       const path = generatePath(typeof to === 'string' ? to : to.pathname, params || ({} as any))
       return (
-        <Link {...props} to={typeof to === 'string' ? path : { pathname: path, search: to.search, hash: to.hash }} />
+        <Link
+          ref={ref}
+          {...props}
+          to={typeof to === 'string' ? path : { pathname: path, search: to.search, hash: to.hash }}
+        />
       )
-    },
+    }),
     Navigate: <P extends Path | To<Path>>({ to, params, ...props }: LinkProps<P, Params>) => {
       const path = generatePath(typeof to === 'string' ? to : to.pathname, params || ({} as any))
       return (
